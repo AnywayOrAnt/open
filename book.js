4a50b252943d264e151cf58bfddfb002e350ccc5
AnywayOrAnt <156141754+AnywayOrAnt@users.noreply.github.com>
2024-09-16 11:34:29 +0900

    Update book.js

--- book.js
+++ book.js
@@ -36,8 +36,9 @@ function textarea2div(){
         h = e.textContent.trim();
         
         if(
-            (new String(h).startsWith("<style>")) ||
-            (new String(h).startsWith("<!DOCTYPE html>"))
+            (new String(h).startsWith("<style")) ||
+            (new String(h).startsWith("<!DOCTYPE html>")) ||
+            (new String(h).startsWith("<script"))
         ){
             h = "    " + h
         }
